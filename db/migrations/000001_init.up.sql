453e36924ede1aa6e746c3150dc852cc304c9787
x1unix <denis0051@gmail.com>
2021-01-11 14:40:03 +0200

    Drop unnecessary index on email col

--- db/migrations/000001_init.up.sql
+++ db/migrations/000001_init.up.sql
@@ -12,9 +12,6 @@ CREATE TABLE "users" (
     "password" CHAR(60) NOT NULL
 );
 
--- User email index, email lookup used very frequently (by auth middleware)
-CREATE UNIQUE INDEX user_emails_idx ON users(email);
-
 -- Groups table
 --
 -- Each group is owned by one user
@@ -62,8 +59,22 @@ CREATE TABLE "loans" (
     "debtor_id" uuid NOT NULL,
     "amount" integer NOT NULL DEFAULT 0,
 
-    -- TODO: select proper on delete strategy
+    -- I added "ON DELETE CASCADE" constraint just for DB consistency.
+    -- API never removes created users from `users` table.
     FOREIGN KEY (lender_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (debtor_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
+-- I intentionally didn't create any indexes for "loans".
+--
+-- Although we have a frequent search by "lender_id" or "debtor_id",
+-- but assuming that table insert/update is a frequent operation - keeping
+-- 2 indexes for huge table will bring more disadvantages than advantages.
+-- (update time and storage space will dramatically increase)
+--
+-- I could try to use composed cluster index, but:
+--  1. Table reordering during update takes time
+--  2. That probably won't speed search by individual field (`lender_id` or `debtor_id`)
+--
+-- Assuming that I do a long search query only once (on balance cache population),
+-- insert time looks more important so I decided to omit index creation.
