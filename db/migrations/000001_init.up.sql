413ce3fad0ccc3bdf7f85f44f0c6807b59d819de
x1unix <denis0051@gmail.com>
2021-01-11 14:52:09 +0200

    Update schema

--- db/migrations/000001_init.up.sql
+++ db/migrations/000001_init.up.sql
@@ -5,7 +5,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- Users table
 --
--- Email limit is 254 chars according to RFC 5321.
+-- Email limit is 254 chars according to RFC 5321, don't ask me who need such long mail.
 -- Password is encrypted using bcrypt, which is always has 60 chars.
 CREATE TABLE "users" (
     "id" uuid PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
